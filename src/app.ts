3fa276f39cf11f24e7925656ebe8b6193ec6556e
gregstoll <greg@gregstoll.com>
2021-09-15 10:53:35 -0500

    show words better and show best letters to guess

--- src/app.ts
+++ src/app.ts
@@ -12,17 +12,59 @@
     async function fetchData(pattern: string, absent_letters: string) {
         let response = await fetch(getURL()+'?pattern='+encodeURIComponent(pattern)+'&absent_letters='+encodeURIComponent(absent_letters));
         const json = await response.json();
-        let word_list = document.getElementById("possible_word_list") as HTMLUListElement;
-        let children: HTMLElement[] = [];
+        let word_list = document.getElementById("possible_word_list");
+        let best_letters_to_guess = document.getElementById("best_letters_to_guess");
+        // TODO - handle error
+        if (json.length === 0) {
+            word_list.innerHTML = "No words found";
+            best_letters_to_guess.innerHTML = "No words found";
+            return;
+        }
+        let word_table = document.createElement("table");
+        let total_frequency = 0;
+        let letter_frequency : Map<string, number> = new Map();
+        for (let char of Array.from({ length: 26 }, (_, i) => String.fromCharCode('a'.charCodeAt(0) + i))) {
+            letter_frequency.set(char, 0);
+        }
+        for(let pattern_char of pattern) {
+            letter_frequency.delete(pattern_char);
+        }
+        for(let absent_char of absent_letters) {
+            letter_frequency.delete(absent_char);
+        }
         for (let result of json) {
-            let result_item = document.createElement("li");
-            result_item.innerHTML = result["word"] + " " + result["frequency"];
-            children.push(result_item);
+            let row = document.createElement("tr");
+            let word_td = document.createElement("td");
+            word_td.appendChild(document.createTextNode(result.word));
+            for (let pattern_char of result.word) {
+                if (letter_frequency.has(pattern_char)) {
+                    letter_frequency.set(pattern_char, letter_frequency.get(pattern_char) + result.frequency);
+                }
+            }
+            let freq_td = document.createElement("td");
+            freq_td.classList.add("frequency");
+            freq_td.appendChild(document.createTextNode(result.frequency));
+            total_frequency += result.frequency;
+            row.appendChild(word_td);
+            row.appendChild(freq_td);
+            word_table.appendChild(row);
         }
+
+        let letter_table = document.createElement("table");
+        Array.from(letter_frequency.entries()).sort((a, b) => b[1] - a[1]).forEach(([letter, frequency]) => {
+            let row = document.createElement("tr");
+            let letter_td = document.createElement("td");
+            letter_td.appendChild(document.createTextNode(letter));
+            let freq_td = document.createElement("td");
+            freq_td.appendChild(document.createTextNode((frequency/total_frequency*100).toFixed(2) + "%"));
+            row.appendChild(letter_td);
+            row.appendChild(freq_td);
+            letter_table.appendChild(row);
+        });
+        best_letters_to_guess.innerHTML = "";
+        best_letters_to_guess.appendChild(letter_table);
         word_list.innerHTML = "";
-        for (let child of children) {
-            word_list.appendChild(child);
-        }
+        word_list.appendChild(word_table);
     }
 
     document.getElementById("search").addEventListener("click", function() {
