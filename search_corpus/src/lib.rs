5f368046ef379e92fc5098a214127d813cded679
Greg Stoll <greg@gregstoll.com>
2025-10-22 20:53:06 -0500

    add support for Cryptogram mode

--- search_corpus/src/lib.rs
+++ search_corpus/src/lib.rs
@@ -22,6 +22,7 @@ const FST_QUESTION_MARK_THRESHOLD: usize = 6;
 pub enum PatternMode {
     WheelOfFortune,
     Crossword,
+    Cryptogram,
 }
 
 impl TryFrom<&str> for PatternMode {
@@ -31,6 +32,7 @@ impl TryFrom<&str> for PatternMode {
         match value {
             "WheelOfFortune" => Ok(PatternMode::WheelOfFortune),
             "Crossword" => Ok(PatternMode::Crossword),
+            "Cryptogram" => Ok(PatternMode::Cryptogram),
             _ => Err(()),
         }
     }
@@ -87,6 +89,32 @@ pub fn search_combinations(parts: &[Vec<char>]) -> Result<Vec<(String, u64)>, St
     Ok(results)
 }
 
+fn is_valid_string(s: &str, pattern: &str, mode: &PatternMode) -> bool {
+    if mode == &PatternMode::Cryptogram {
+        // the regex crate doesn't support backreferences, so make sure
+        // that capital letters in the pattern match in the result string
+        // TODO - also make sure that no mappings are reused?
+        let mut mappings: HashMap<char, char> = HashMap::new();
+        let s_chars = s.chars().collect::<Vec<_>>();
+        for (i, pattern_char) in pattern.char_indices() {
+            if pattern_char.is_ascii_uppercase() {
+                let entry = mappings.entry(pattern_char);
+                match entry {
+                    std::collections::hash_map::Entry::Occupied(occupied_entry) => {
+                        if &s_chars[i] != occupied_entry.get() {
+                            return false;
+                        }
+                    }
+                    std::collections::hash_map::Entry::Vacant(vacant_entry) => {
+                        vacant_entry.insert(s_chars[i]);
+                    }
+                }
+            }
+        }
+    }
+    return true;
+}
+
 pub fn process_query_string(query: &str) -> Result<json::JsonValue, String> {
     let query_parts: HashMap<String, String> = url::form_urlencoded::parse(query.as_bytes())
         .into_owned()
@@ -99,7 +127,7 @@ pub fn process_query_string(query: &str) -> Result<json::JsonValue, String> {
     let pattern = query_parts
         .get("pattern")
         .ok_or(String::from("Internal error - no pattern specified!"))?;
-    validate_pattern(pattern)?;
+    validate_pattern(pattern, &mode)?;
     // TODO - validate if in WheelOfFortune mode?
     //let absent_letters = query_parts.get("absent_letters").ok_or(String::from("Internal error - no absent_letters specified!"))?;
     let empty_absent_letters = String::from("");
@@ -108,7 +136,11 @@ pub fn process_query_string(query: &str) -> Result<json::JsonValue, String> {
         .unwrap_or(&empty_absent_letters);
     validate_absent_letters(absent_letters)?;
     let word_regex = build_regex(pattern, absent_letters, &mode)?;
-    let pattern_question_marks = pattern.chars().filter(|c| *c == '?').count();
+    let pattern_question_marks = if mode == PatternMode::Cryptogram {
+        pattern.chars().filter(|c| c.is_ascii_uppercase()).count()
+    } else {
+        pattern.chars().filter(|c| *c == '?').count()
+    };
     let read_fst_file: bool = pattern_question_marks < FST_QUESTION_MARK_THRESHOLD;
     let json_results = if read_fst_file {
         let mmap = unsafe {
@@ -130,6 +162,12 @@ pub fn process_query_string(query: &str) -> Result<json::JsonValue, String> {
             .into_stream()
             .into_str_vec()
             .map_err(|e| e.to_string())?;
+        if mode == PatternMode::Cryptogram {
+            results = results
+                .into_iter()
+                .filter(|a| is_valid_string(&a.0, &pattern, &mode))
+                .collect();
+        }
         results.sort_by(|a, b| b.1.cmp(&a.1).then(a.0.cmp(&b.0)));
         results
             .iter()
@@ -145,8 +183,10 @@ pub fn process_query_string(query: &str) -> Result<json::JsonValue, String> {
             let mut parts = line.split_ascii_whitespace();
             let word = parts.next().unwrap();
             if word_regex.is_match(word) {
-                // TODO - use Object constructor
-                results.push(json::object! { "word" => word, "frequency" => parts.next().unwrap().parse::<u64>().unwrap() });
+                if !(mode == PatternMode::Cryptogram && is_valid_string(&word, &pattern, &mode)) {
+                    // TODO - use Object constructor
+                    results.push(json::object! { "word" => word, "frequency" => parts.next().unwrap().parse::<u64>().unwrap() });
+                }
             }
             line.clear();
         }
@@ -159,54 +199,86 @@ fn build_regex(pattern: &str, absent_letters: &str, mode: &PatternMode) -> Resul
     // (?-u) turns off unicode, although that's not really necessary here since we're already
     // specifying the exact characters to match.
     let mut regex_str = "^(?-u)".to_string();
-    if *mode == PatternMode::WheelOfFortune {
-        let mut absent_letter_set = HashSet::new();
-        for letter in absent_letters.chars() {
-            //println!("inserting {} from absent", letter);
-            absent_letter_set.insert(letter.to_ascii_lowercase());
-        }
-        for letter in pattern.chars() {
-            if letter.is_ascii_alphabetic() {
-                //println!("inserting {} from pattern", letter);
+    match mode {
+        &PatternMode::WheelOfFortune => {
+            let mut absent_letter_set = HashSet::new();
+            for letter in absent_letters.chars() {
+                //println!("inserting {} from absent", letter);
                 absent_letter_set.insert(letter.to_ascii_lowercase());
             }
+            for letter in pattern.chars() {
+                if letter.is_ascii_alphabetic() {
+                    //println!("inserting {} from pattern", letter);
+                    absent_letter_set.insert(letter.to_ascii_lowercase());
+                }
+            }
+            // regex gets cranky about turning off unicode then matching characters that aren't something
+            // (because they might be unicode characters!) so just iterate over all the possibilities here.
+            let mut absent_letter_builder = "[".to_string();
+            for letter in 'a'..='z' {
+                if !absent_letter_set.contains(&letter) {
+                    absent_letter_builder.push(letter);
+                }
+            }
+            absent_letter_builder.push(']');
+            regex_str.push_str(
+                &pattern
+                    .to_ascii_lowercase()
+                    .replace("?", absent_letter_builder.as_str()),
+            );
         }
-        // regex gets cranky about turning off unicode then matching characters that aren't something
-        // (because they might be unicode characters!) so just iterate over all the possibilities here.
-        let mut absent_letter_builder = "[".to_string();
-        for letter in 'a'..='z' {
-            if !absent_letter_set.contains(&letter) {
+        &PatternMode::Crossword => {
+            let mut absent_letter_builder = "[".to_string();
+            for letter in 'a'..='z' {
                 absent_letter_builder.push(letter);
             }
+            absent_letter_builder.push(']');
+            regex_str.push_str(
+                &pattern
+                    .to_ascii_lowercase()
+                    .replace("?", absent_letter_builder.as_str()),
+            );
         }
-        absent_letter_builder.push(']');
-        regex_str.push_str(
-            &pattern
-                .to_ascii_lowercase()
-                .replace("?", absent_letter_builder.as_str()),
-        );
-    } else {
-        let mut absent_letter_builder = "[".to_string();
-        for letter in 'a'..='z' {
-            absent_letter_builder.push(letter);
+        &PatternMode::Cryptogram => {
+            let mut known_letters: HashSet<char> = pattern
+                .chars()
+                .filter(|c| !c.is_ascii_uppercase())
+                .collect();
+            for letter in absent_letters.chars() {
+                //println!("inserting {} from absent", letter);
+                known_letters.insert(letter.to_ascii_lowercase());
+            }
+            for pattern_char in pattern.chars() {
+                if !pattern_char.is_ascii_uppercase() {
+                    regex_str.push(pattern_char);
+                } else {
+                    regex_str.push('[');
+                    for letter in 'a'..='z' {
+                        // cryptogram rules - can't match this character
+                        if !(known_letters.contains(&letter)
+                            || letter == pattern_char.to_ascii_lowercase())
+                        {
+                            regex_str.push(letter);
+                        }
+                    }
+                    regex_str.push(']');
+                }
+            }
         }
-        absent_letter_builder.push(']');
-        regex_str.push_str(
-            &pattern
-                .to_ascii_lowercase()
-                .replace("?", absent_letter_builder.as_str()),
-        );
     }
     regex_str.push('$');
     Regex::new(&regex_str).map_err(|e| e.to_string())
 }
 
-fn is_allowed_char(c: char) -> bool {
-    c.is_ascii_alphabetic() || c == '\'' || c == '-' || c == '?'
+fn is_allowed_char(c: char, mode: &PatternMode) -> bool {
+    if c.is_ascii_alphabetic() || c == '\'' || c == '-' {
+        return true;
+    }
+    return mode != &PatternMode::Cryptogram && c == '?';
 }
 
-fn validate_pattern(pattern: &str) -> Result<(), String> {
-    if pattern.chars().any(|c| !is_allowed_char(c)) {
+fn validate_pattern(pattern: &str, mode: &PatternMode) -> Result<(), String> {
+    if pattern.chars().any(|c| !is_allowed_char(c, &mode)) {
         return Err("Disallowed characters in pattern".to_string());
     }
     if pattern.len() > 20 {
@@ -400,6 +472,50 @@ mod tests {
         let query = format!("mode=WheelOfFortune&pattern={}&absent_letters=h", too_long);
         let result = process_query_string(&query);
         assert!(!result.is_ok());
+    }
+
+    #[test]
+    fn test_cryptogram_no_known() {
+        let query = format!("mode=Cryptogram&pattern=ABC&absent_letters=");
+        let result = process_query_string(&query).unwrap();
+        assert_eq!("the", result[0]["word"].to_string());
+    }
+
+    #[test]
+    fn test_cryptogram_no_known_but_letter_does_not_match() {
+        let query = format!("mode=Cryptogram&pattern=TBC&absent_letters=");
+        let result = process_query_string(&query).unwrap();
+        // can't be "the" because T can't map to t
+        assert_eq!("and", result[0]["word"].to_string());
+    }
+
+    #[test]
+    fn test_cryptogram_no_known_with_repeated_letters() {
+        let query = format!("mode=Cryptogram&pattern=ABCC&absent_letters=");
+        let result = process_query_string(&query).unwrap();
+        assert_eq!("will", result[0]["word"].to_string());
+    }
+
+    #[test]
+    fn test_cryptogram_a_few_known() {
+        let query = format!("mode=Cryptogram&pattern=XBch&absent_letters=");
+        let result = process_query_string(&query).unwrap();
+        assert_eq!("such", result[0]["word"].to_string());
+    }
+
+    #[test]
+    fn test_cryptogram_do_not_reuse_letters() {
+        let query = format!("mode=Cryptogram&pattern=XBCt&absent_letters=");
+        let result = process_query_string(&query).unwrap();
+        // not "that" because t is already used
+        assert_eq!("what", result[0]["word"].to_string());
+    }
+
+    #[test]
+    fn test_cryptogram_a_few_absent() {
+        let query = format!("mode=Cryptogram&pattern=ABC&absent_letters=ea");
+        let result = process_query_string(&query).unwrap();
+        assert_eq!("for", result[0]["word"].to_string());
     }
 
     #[test]
